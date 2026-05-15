2fc650a73aac207889d24895ffb4c412155e9daa
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-05-14 20:41:02 -0500

    Added google-ai functionality to main.py as part of the assignment

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+
 def main():
     print("Hello from aiagent!")
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if not api_key:
+    raise RuntimeError("API key not found")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
 
 if __name__ == "__main__":
     main()
