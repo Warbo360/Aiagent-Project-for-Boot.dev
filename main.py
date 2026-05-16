3bf0ec5c29313007d3e3a10909d55bb76ce9e54c
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-05-16 13:48:19 -0500

    Made it so the agent takes CLI user args instead of hard coding prompts, also program exits with error code and help message if no arg is given.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import os
-from google import genai
+import argparse
 from dotenv import load_dotenv
+from google import genai
+import os
 
 
 def main():
     print("Hello from aiagent!")
+
+parser = argparse.ArgumentParser(description = "Chatbot")
+parser.add_argument("user_prompt", type = str, help = "User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,16 +18,15 @@ if not api_key:
     raise RuntimeError("API key not found")
 
 client = genai.Client(api_key=api_key)
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 response = client.models.generate_content(
     model = 'gemini-2.5-flash',
-    contents = prompt
+    contents = args.user_prompt
 )
 
 if response.usage_metadata:
     print(
-            f"\nUser prompt: {prompt}\n\n"
+        f"\nUser prompt: {args.user_prompt}\n\n"
         f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
         f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
     )
