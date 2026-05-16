4c69ffed01d24d8891a38142d7cff2813dd4aba3
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-05-16 17:01:13 -0500

    So past implementation of validation of directories was faulty so I reworked that once I better understood why.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,17 +1,21 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    working_dir_abs = os.path.abspath(working_directory)
+
     try:
-        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        working_dir_abs = os.path.abspath(working_directory)
     except:
+        return f"Error: \"{working_directory}\" is not a directory"
+
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+    valid_target_dir = None
+    if os.path.isdir(target_dir):
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    else:
         return f"Error: \"{directory}\" is not a directory"
 
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
-
-    if not os.path.isdir(directory):
-        return f"Error: \"{directory}\" is not a directory"
-    elif valid_target_dir:
+    if valid_target_dir:
         return f"Success: \"{directory}\" is within the working directory"
     else:
-        return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
+        return f"Error: Cannot list \"{directory}\" as it is outside the working directory"
